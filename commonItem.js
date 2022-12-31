ffb207f215dc4ad2705841897145fdffbacdb60f
Isaac4d.com <izac3d@gmail.com>
2022-12-31 20:25:01 +0200

    containsCommonItems2

---
+++ commonItem.js
@@ -0,0 +1,30 @@
+const array1 = ['z', 'x', 'y', 'd'];
+const array2 = ['x', 't', 's'];
+/**
+ * 
+ * @param {Array} arr1 
+ */
+
+function containsCommonItems(arr1, arr2) {
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+      if (!map[arr1[i]]) {
+        item = arr1[i];
+        map[item] = true;
+      } 
+    }
+    
+    for (let j = 0; j < arr2.length; j++){
+      if (map[arr2[j]]) {
+       return true; 
+      }
+    }
+    return false;
+
+}
+
+//console.log(containsCommonItems(array1, array2));
+
+function containsCommonItems(arr1, arr2) {
+  return arr1
+}
